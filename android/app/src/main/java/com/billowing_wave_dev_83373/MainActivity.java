27eb23e86d2f87be99f55fcfd692a5a623341f1f
Crowdbotics <engineering@crowdbotics.com>
2023-05-15 11:13:28 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/billowing_wave_dev_83373/MainActivity.java
@@ -0,0 +1,15 @@
+package com.billowing_wave_dev_83373;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "billowing_wave_dev_83373";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "billowing-wave-dev-83373",
+  url: "https://billowing-wave-dev-83373.botics.co",
+  api: "https://billowing-wave-dev-83373.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
